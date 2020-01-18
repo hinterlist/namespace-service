df2c3322573cf71549c3d2be1050b7c82beb9eaa
Dimitry Kislichenko <dimitry.kislichenko@gmail.com>
2020-01-18 22:46:42 +0200

    Add express and template for server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+
+app.listen(port, () => {
+  console.log(`🚀 Server ready at http://www.localhost:${port}`);
+});
