8cbcbd53517e50cb976aa654be9c007f1952fc5b
Dimitry Kislichenko <dimitry.kislichenko@gmail.com>
2020-01-19 01:51:48 +0200

    Log generated IDs

--- src/controllers/newid.controller.ts
+++ src/controllers/newid.controller.ts
@@ -1,11 +1,14 @@
-import { redis } from '../providers';
+import { redis, logger } from '../providers';
 import { Request, Response } from 'express';
 
 const handler = async (req: Request, res: Response) => {
   const namespace = req.params.namespace;
   const nextId = await redis.incr(namespace);
+  const value = `${namespace}-${nextId}`;
 
-  res.send(`${namespace}-${nextId}`);
+  logger.info(`Generated new ID: ${value}`);
+
+  res.send(value);
 };
 
 export default handler;
